9b46b101673190136924bf174048892b12a493b1
ChenulaJ <chenula.20211118@iit.ac.lk>
2023-03-14 20:46:00 +0530

    Added the text to speech setup

--- run.py
+++ run.py
@@ -12,3 +12,11 @@ from object_detection.utils import label_map_util                          # To 
 from object_detection.utils import config_util                             # To load model pipeline.
 from object_detection.utils import visualization_utils as viz_utils        # To draw rectangles.
 from object_detection.builders import model_builder 
+
+#Text to speech setup.
+engine = pyttsx3.init()
+en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"  # female
+ru_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0"  # male
+engine.setProperty('voice', en_voice_id)
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 25)

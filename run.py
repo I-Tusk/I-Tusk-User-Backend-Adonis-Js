bc875a0db46dd9b93e362968f847072d164fd431
Shaun <124687868+shaunyogeshwaran@users.noreply.github.com>
2023-04-06 00:12:44 +0530

    added the category_index function

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# To Perform OS level works.
-import os
-import six
 # OpenCV for Computer Vision                                                        # It has a dictionary that contains colors for each label
 import cv2
 # To get arguments
@@ -17,11 +14,15 @@ import playsound
 import tensorflow as tf
 # To handle label map.
 from object_detection.utils import label_map_util
-# To load model pipeline.
+# To load the model pipeline.
 from object_detection.utils import config_util
-# To draw rectangles.
+# To draw the rectangles.
 from object_detection.utils import visualization_utils as viz_utils
+# To load & Build the models.
 from object_detection.builders import model_builder
+import six
+# To Perform OS level works.
+mport os
 
 # Text to speech setup.
 engine = pyttsx3.init()
@@ -31,35 +32,39 @@ engine.setProperty('voice', en_voice_id)
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate - 25)
 
-#Comment 3
+
 def talk_function(text):               # Text to speech convertion
     print("Computer: {}".format(text))
     engine.say(text)
     engine.runAndWait()
 
-    
 
-# Enable GPU dynamic memory allocation
+# Enable the GPU dynamic memory allocation
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-model_config_path =  f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/pipeline.config'        # Store the path of config file
-checkpoint_model_path   =  f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/checkpoint/ckpt-0'      # Store the path of model
-label_map_path    =  f'data/mscoco_label_map.pbtxt'                             # Store the path of label_map 
+
+# Store the path of config file
+model_config_path = f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/pipeline.config'
+# Store the path of model
+checkpoint_model_path = f'data/models/ssd_mobilenet_v2_320x320_coco17_tpu-8/checkpoint/ckpt-0'
+# Store the path of label_map
+label_map_path = f'data/mscoco_label_map.pbtxt'
 
 
 # Load pipeline config and build a detection model
 configs = config_util.get_configs_from_pipeline_file(model_config_path)
 model_config = configs['model']
-detection_model = model_builder.build(model_config=model_config, is_training=False)
+detection_model = model_builder.build(
+    model_config=model_config, is_training=False)
 
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
 ckpt.restore(checkpoint_model_path).expect_partial()
 
-@tf.function
 
+@tf.function
 def detect_fn(image):
     """Detect objects in image."""
 
@@ -68,3 +73,7 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
 
     return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+
+category_index = label_map_util.create_category_index_from_labelmap(label_map_path,
+                                                                    use_display_name=True)

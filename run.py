4bffcdd1caad172ea3d8136fc76b5eba1ee1590f
ChenulaJ <chenula.20211118@iit.ac.lk>
2023-04-07 13:41:27 +0530

    Added the code to to draw the box over elephant

--- run.py
+++ run.py
@@ -132,3 +132,5 @@ for box, color in box_to_display_str_map.items():
         h = ymax - ymin
 
         if box_to_display_str_map[box][0].replace("_"," ") == "elephant": # Get only label name not the total number of items
+
+         cv2.rectangle(image_np_with_detections, (int(x),int(y)), (int(x) + int(w), int(y) + int(h)), (0, 0, 255), 4)
